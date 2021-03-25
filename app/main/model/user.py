a358d0b97e8f6fccae95a87075a15ff1ebca2b7a
truongnhthtb@gmail.com <truongnhthtb@gmail.com>
2021-03-25 17:22:49 +0700

    crud user and login/logout

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -1,5 +1,10 @@
 from enum import unique
+import datetime
+import jwt
+from app.main.model.blacklist import BlacklistToken
+from ..config import key
 from .. import db, flask_bcrypt
+from typing import Union
 
 class User(db.Model):
     """ User Model for storing user related details """
@@ -23,6 +28,45 @@ class User(db.Model):
         
     def check_password(self, password):
         return flask_bcrypt.check_password_hash(self.password_hash, password)
+    
+    @staticmethod
+    def encode_auth_token(user_id: int) -> bytes:
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+        
+    @staticmethod
+    def decode_auth_token(auth_token: str) -> Union[str, int]:
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
     
     def __repr__(self):
         return "<User '{}'".format(self.username)
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -16,10 +16,10 @@ def save_new_user(data):
             registered_on=datetime.datetime.utcnow()
         )
         save_changes(new_user)
-        response_object = {
+        response_object ={
             'status': 'success',
-            'message': 'Successfully registered'
-        }
+            'message': 'Register user successfully',
+        } 
         return response_object, 201
     else:
         response_object = {
@@ -35,6 +35,24 @@ def get_all_users():
 def get_a_user(public_id):
     return User.query.filter_by(public_id=public_id).first()
         
+        
+def generate_token(user):
+    try:
+        # generate the auth token
+        auth_token = user.encode_auth_token(user.id)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully registered.',
+            'Authorization': auth_token.decode()
+        }
+        return response_object, 201
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': 'Some error occurred. Please try again.'
+        }
+        return response_object, 401
+    
 def save_changes(data):
     db.session.add(data)
     db.session.commit()
